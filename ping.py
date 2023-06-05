ff5bd9a20aeda6396022edfa429f0d7ef76c5ddd
Yan-Ying Liao <n9102125@gmail.com>
2023-06-05 20:26:57 +0200

    ping go-germany as well

--- ping.py
+++ ping.py
@@ -1,10 +1,13 @@
 import requests
 
-URL = 'https://aachen-termin-bot.onrender.com'
+URL_TERMIN_BOT = 'https://aachen-termin-bot.onrender.com'
+URL_GO_GERMANY = 'https://go-germany-api.onrender.com/status'
 
 def ping():
-    r = requests.get(URL)
-    print(r)
+    r1 = requests.get(URL_TERMIN_BOT)
+    r2 = requests.get(URL_GO_GERMANY)    
+    print(r1)
+    print(r2)
 
 if __name__ == '__main__':
     ping()
