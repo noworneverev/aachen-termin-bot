96167a30090253ec2c5bbe54f9c55ab1f27e0fe7
Yan-Ying Liao <n9102125@gmail.com>
2023-09-25 12:15:19 +0200

    fix deploy issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,17 @@ import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 import { Container, Header } from "semantic-ui-react";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Router basename="/frontend">
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+    </Router>
+  );
+  // return <Home />;
 }
 
 export default App;
