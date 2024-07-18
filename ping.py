078145406a8faadf32c2e8d9fa85d42a981b8a30
Yan-Ying Liao <n9102125@gmail.com>
2024-07-18 14:41:43 +0200

    Restart server every hour

--- ping.py
+++ ping.py
@@ -6,8 +6,9 @@ URL_GO_GERMANY = 'https://go-germany-api.onrender.com/status'
 def ping():
     r1 = requests.get(URL_TERMIN_BOT)
     r2 = requests.get(URL_GO_GERMANY)    
-    print(r1)
-    print(r2)
+    print(f"Ping results for {URL_TERMIN_BOT} and {URL_GO_GERMANY}:")
+    print(f"Aachen Termin Bot: {r1.status_code}")
+    print(f"Go Germany: {r2.status_code}")    
 
 if __name__ == '__main__':
-    ping()
+    ping()    
